ab3af71b48a80a4dc6c5baf41624d2e78a482bd4
alemb8 <figueiraalena@gmail.com>
2024-10-05 16:17:31 -0400

    Create nothing complete.js

---
+++ nothing complete.js
@@ -0,0 +1,123 @@
+var arrayOfColor= [
+  'blue'
+  'orange'
+  ];
+var ramdomCOlor = pickRandom(arrayOfColor)
+
+  //Variables
+
+  var myExmple;
+myExample = 'I created the value';
+print(myExample);
+
+//Arrays
+var exampleArray = [
+  'blue'
+  'orange'
+  5,
+  10,
+  [
+  50,
+  10
+  ]
+
+// if statement
+var aNumber = 5;
+if(aNumber === 5);
+{
+  if (aNumber==4) 
+  {
+    drawBox(red)
+  }
+
+  //User Survey
+  var answer = pickRmdom 
+    ([
+    'head',
+    'tails'.
+    )];
+  Print (answer);
+  If (anwer === 'heads')
+    {
+    drawBox ('gvg ggggwg');
+    }
+
+    //Arothmetic operators
+var wackyArithmeic = 5*5/-5+5;
+var fullName = 'Johnny' + 'Applessed';
+
+var x=5;
+  x=x+3;
+  print ('x is'+x)
+    x=-4;
+  print ('x us now' + x);
+
+//Used a logical operator
+if (time === 'midnight' && dsay ! === 'saturday')
+{
+  print ('you should go to bed')
+}
+
+//Used for loops
+  var grocries = 
+    [
+      'apples'
+      'yogurt'
+      'toothpaste'
+  ];
+  for (var element of groceries)
+    {
+      print (element);
+    }
+
+  for (var; = 0; i < 10; i = i + 1)
+  {
+    >1
+    >2
+    >3
+    >4
+  }
+///Clasic for a loop
+  for (i = 2: i < 7; i+1)
+  {
+    print(i);
+  }
+
+// Nested Loop
+  fro (var outeNumber of
+  [
+      1,
+      2,
+      3,
+  ])
+  fro (var nestedNumber of {
+      4,
+      5,
+      6,
+      ])
+        print(outerNumber, nestedNumber);
+  }]
+//Access a property
+  vvar objectName = {
+    propertyName: 'Coding is fun.' , otherPropertyName;
+  'This is also a key value'
+}
+print (objectName.propertyName);
+
+//A value store in the variable
+var teacup = {
+  red:100,
+  green: 50,
+  Blue:50,
+};
+var sugar = teacup.blue
+
+var myBackpack = {
+  food: [
+    'bananas',
+    'pepsi'
+    ]
+    Equipment: [
+  'map',
+  'something else',
+  ]}; print (myBackpad, food);
